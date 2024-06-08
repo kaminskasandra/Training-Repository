7aec61212c1fe4d42ea6e86e8d263855f29e4399
kaminskasandra <san.porombka@gmail.com>
2024-06-08 11:42:48 +0200

    new learning project

---
+++ src/main/java/com/example/learningtasks/LearningTasksApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningTasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningtasks/LearningTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
