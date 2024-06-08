bf39239bf4484fd44c96a3fdc32836d093c1b0e9
kaminskasandra <san.porombka@gmail.com>
2024-06-08 12:05:06 +0200

    fibonacci task

---
+++ src/main/java/com/example/learningtasks/Fibonacci.java
@@ -0,0 +1,22 @@
+package com.example.learningtasks;
+
+public class Fibonacci {
+
+    public int fib(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static int fibonacci(int n) {
+        int a = 0, b = 1, sum;
+        for(int i = 2; i <= n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return b;
+    }
+}
