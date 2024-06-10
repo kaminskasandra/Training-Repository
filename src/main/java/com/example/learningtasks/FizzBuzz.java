5a21c90b0dc545d334e48d267b0d96e5f62caaed
kaminskasandra <san.porombka@gmail.com>
2024-06-10 15:43:27 +0200

    FizzBuzz challenge

---
+++ src/main/java/com/example/learningtasks/FizzBuzz.java
@@ -0,0 +1,34 @@
+package com.example.learningtasks;
+
+public class FizzBuzz {
+
+    public void fizzBuzzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void fizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else {
+                    System.out.println("Fizz");
+                }
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
