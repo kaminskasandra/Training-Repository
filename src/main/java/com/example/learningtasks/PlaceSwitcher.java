5409ae623b3db77b8af1e2605f1042e85bef4a59
kaminskasandra <san.porombka@gmail.com>
2024-06-12 12:15:00 +0200

    Switch place - challenge

---
+++ src/main/java/com/example/learningtasks/PlaceSwitcher.java
@@ -0,0 +1,18 @@
+package com.example.learningtasks;
+
+public class PlaceSwitcher {
+
+    public void switchPlace(int a, int b) {
+        int c = a;
+        a = b;
+        b = c;
+        System.out.println("a = " + a + " b = " + b);
+    }
+
+    public void switchPlaceWithoutExtraVariable(int a, int b) {
+       a = b - a;
+       b = b - a;
+       a = a + b;
+       System.out.println("a = " + a + " b = " + b);
+    }
+}
