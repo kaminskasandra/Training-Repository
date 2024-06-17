4910873aef18a7c760ea14d75f19cdb9713ae3e7
kaminskasandra <san.porombka@gmail.com>
2024-06-17 10:13:06 +0200

    Maximum minimum - challenge

---
+++ src/main/java/com/example/learningtasks/MaximumMinimum.java
@@ -0,0 +1,47 @@
+package com.example.learningtasks;
+
+public class MaximumMinimum {
+
+    public Integer findSecondMaximum(int[] numbers) {
+
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num : numbers) {
+            if (num > firstMax) {
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num < firstMax && num > secondMax) {
+                secondMax = num;
+            }
+        }
+
+        if (secondMax == Integer.MIN_VALUE) {
+            return null;
+        }
+
+        return secondMax;
+    }
+
+    public Integer findSecondMinimum(int[] numbers) {
+
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int num : numbers) {
+            if (num < firstMin) {
+                secondMin = firstMin;
+                firstMin = num;
+            } else if (num > firstMin && num < secondMin) {
+                secondMin = num;
+            }
+        }
+
+        if (secondMin == Integer.MAX_VALUE) {
+            return null;
+        }
+
+        return secondMin;
+    }
+}
+
