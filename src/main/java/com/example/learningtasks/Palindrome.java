9810e948be98eb165ada2ddd017b2ef2b9943550
kaminskasandra <san.porombka@gmail.com>
2024-07-02 09:40:15 +0200

    Palindrome challenge

---
+++ src/main/java/com/example/learningtasks/Palindrome.java
@@ -0,0 +1,16 @@
+package com.example.learningtasks;
+
+public class Palindrome {
+    public boolean isPalindrome(String str) {
+    int left = 0;
+    int right = str.length() - 1;
+    while (left < right) {
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+}
