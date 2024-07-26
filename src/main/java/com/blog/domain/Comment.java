a94a6f5bc3d2564fb9deac30b3165c494c275b1b
kaminskasandra <san.porombka@gmail.com>
2024-07-26 12:41:03 +0200

    Comment entity

--- src/main/java/com/blog/domain/Comment.java
+++ src/main/java/com/blog/domain/Comment.java
@@ -1,4 +1,15 @@
 package com.blog.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name = "COMMENTS")
 public class Comment {
 }
