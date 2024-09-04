609bc0264bcb68e2527e6eaeab9f86acb18f8769
kaminskasandra <san.porombka@gmail.com>
2024-09-04 13:18:40 +0200

    category controller

--- src/main/java/com/blog/controller/CategoryController.java
+++ src/main/java/com/blog/controller/CategoryController.java
@@ -1,4 +1,16 @@
 package com.blog.controller;
 
+import com.blog.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/controllers")
 public class CategoryController {
+
+    private final ModelMapper modelMapper;
+    private final CategoryService categoryService;
 }
