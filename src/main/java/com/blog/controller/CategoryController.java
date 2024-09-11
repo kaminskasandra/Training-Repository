4388dda7c423b1ca8adc293d2645fef5829500fb
kaminskasandra <san.porombka@gmail.com>
2024-09-11 07:57:15 +0200

    category controller

--- src/main/java/com/blog/controller/CategoryController.java
+++ src/main/java/com/blog/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.blog.controller;
 
+import com.blog.dto.CategoryDto;
 import com.blog.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +18,9 @@ public class CategoryController {
 
     private final ModelMapper modelMapper;
     private final CategoryService categoryService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CategoryDto> saveCategory(@RequestBody CategoryDto categoryDto) {
+        return ResponseEntity.ok(modelMapper.map(categoryService.saveCategory(categoryDto), CategoryDto.class));
+    }
 }
