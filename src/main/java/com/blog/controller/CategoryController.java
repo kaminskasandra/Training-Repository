9179d920775447e9bf7828bf2128dec1684c9d76
kaminskasandra <san.porombka@gmail.com>
2024-09-16 13:17:28 +0200

    category controller

--- src/main/java/com/blog/controller/CategoryController.java
+++ src/main/java/com/blog/controller/CategoryController.java
@@ -1,19 +1,18 @@
 package com.blog.controller;
 
 import com.blog.dto.CategoryDto;
+import com.blog.exceptions.CategoryNotFoundException;
+import com.blog.exceptions.CommentNotFoundException;
 import com.blog.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/controllers")
+@RequestMapping("/v1/categories")
 public class CategoryController {
 
     private final ModelMapper modelMapper;
@@ -22,5 +21,11 @@ public class CategoryController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CategoryDto> saveCategory(@RequestBody CategoryDto categoryDto) {
         return ResponseEntity.ok(modelMapper.map(categoryService.saveCategory(categoryDto), CategoryDto.class));
+    }
+
+    @DeleteMapping(value = "/{categorieId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable long categoryId) throws CategoryNotFoundException {
+        categoryService.deleteCategoryById(categoryId);
+        return ResponseEntity.ok().build();
     }
 }
