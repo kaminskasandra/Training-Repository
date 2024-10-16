f982204f53b74cac84fa73f834a584b8afcb3332
kaminskasandra <san.porombka@gmail.com>
2024-10-16 12:54:00 +0200

    email scheduler

--- src/main/java/com/blog/scheduler/EmailScheduler.java
+++ src/main/java/com/blog/scheduler/EmailScheduler.java
@@ -1,5 +1,8 @@
 package com.blog.scheduler;
 
+import com.blog.domain.Post;
+import com.blog.domain.User;
+import com.blog.dto.UserDto;
 import com.blog.exceptions.UserNotFoundException;
 import com.blog.service.MailService;
 import com.blog.service.PostService;
@@ -8,6 +11,10 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +27,10 @@ public class EmailScheduler {
     @Scheduled(cron = "0 0 7 * * *")
     @Transactional
     public void sendInformationEmail() throws UserNotFoundException {
+        HashMap<String, List<Post>> mailToSend = new HashMap<>();
+        for (UserDto user : userService.getAllUsers()) {
+            List<Post> appointments = new ArrayList<>();
 
+        }
     }
 }
